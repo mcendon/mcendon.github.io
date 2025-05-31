2e559f5dc0cbc89b6b5cc054778930bb938ad507
Mauro Cendon Hidalgo <mau.cendon@gmail.com>
2025-05-31 17:26:58 +0200

    chore: adding angular analog.js and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import analogjsangular from '@analogjs/astro-angular';
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    site: 'https://mcendon.github.io',
+    integrations: [mdx(), sitemap(), analogjsangular()],
 });
